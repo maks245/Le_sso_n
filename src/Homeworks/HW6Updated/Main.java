a18503bf9b3c5e86f6347389c12179c85edf473c
maks245 <mpalamarcuk635@gmail.com>
2023-10-13 23:30:39 +0300

    Updated homework with abstract classes about figures added new formulas to Triangle class

--- src/Homeworks/HW6Updated/Main.java
+++ src/Homeworks/HW6Updated/Main.java
@@ -1,14 +1,17 @@
 package Homeworks.HW6Updated;
 
+
 public class Main {
     public static void main(String[] args) {
-        Figure[] figures = new Figure[3];
+        Figure[] figures = new Figure[5];
         figures[0] = new Square(4.0);
-        figures[1] = new Triangle(4.0, 4.0, 4.0);
-        figures[2] = new Circle(4.0);
-        for (int i = 0; i < figures.length; i++) {
-            System.out.println(figures[i].calculatePerimeter());
-            System.out.println(figures[i].calculateSquare());
+        figures[1] = new Triangle(4, 4, 4);
+        figures[2] = new Triangle(4,2);
+        figures[3] = new Triangle(5,3,7.8);
+        figures[4] = new Circle(4.0);
+        for (Figure figure : figures) {
+            System.out.println(figure.calculatePerimeter());
+            System.out.println(figure.calculateSquare());
         }
 
     }
